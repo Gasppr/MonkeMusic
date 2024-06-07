18dbcf9c4317da812145240051a4073057489518
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-06-07 08:44:37 -0300

    First Commit

---
+++ src/main/java/br/com/gaspp/monkemusic/MonkemusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.gaspp.monkemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkemusicApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkemusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gaspp/monkemusic/MonkemusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gaspp.monkemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkemusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
